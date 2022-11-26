ff090ec4f09a529e192973f0014f0b27d43084bb
bshriyuni <bessesahriyuni@gmail.com>
2022-11-26 15:45:13 +0800

    tugas 8

---
+++ H071211008/PRAKTIKUM-8/database/migrations/2022_11_16_034017_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('fakultas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
