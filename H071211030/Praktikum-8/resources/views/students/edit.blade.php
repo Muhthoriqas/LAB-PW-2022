b3187649c76e18714002fe8fe40bbf72b6b15192
Ramstuy <ikrhidayat@gmail.com>
2022-11-27 23:52:12 +0800

    Praktikum-8

---
+++ H071211030/Praktikum-8/resources/views/students/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('students.layout')
+@section('content')
+  
+<div class="card" style="margin:20px;">
+  <div class="card-header">Edit Student</div>
+  <div class="card-body">
+       
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+    
+  </div>
+</div>
+  
+@stop
