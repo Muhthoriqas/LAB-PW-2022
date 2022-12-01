4c45fe0853b18f3f968d13f1428a134df41468b8
EdgarWi1 <edgarsilvanus12@gmail.com>
2022-12-01 17:56:16 +0800

    delete

--- H071211026/Praktikum-6/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = new mysqli('localhost', 'root', '', 'php6');
-
-if(!$con){
-   die(mysqli_error($con));
-}
-?>
--- H071211026/Praktikum-7/signup/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = new mysqli('localhost', 'root', '', 'php7');
-
-if(!$con){
-   die(mysqli_error($con));
-}
-?>
