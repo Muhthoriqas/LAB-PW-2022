5655bbd5d61e9a4b6c523f3091f846ec371df062
Adhitya Fajar Risqi Djati Pratama <levafr369@gmail.com>
2022-12-15 14:37:51 +0800

    Tugas 8 Adit

---
+++ H071211067/PRAKTIKUM-8/database/migrations/2022_11_21_132531_create_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->string('member');
+            $table->unique('member');
+            $table->string('nama');
+            $table->string('alamat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
