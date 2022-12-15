04da4cbe38f50bc869634de51e2c7a5351d4f1aa
ThoriqAS <72277295+Muhthoriqas@users.noreply.github.com>
2022-12-15 16:44:15 +0800

    Revert "Final"

--- H071211030/Final/resources/views/mail/mail.blade.php
+++
@@ -1,7 +0,0 @@
-<p>Message:</p>
-<p>{{$data['message']}}</p>
-
-<div style="margin-top: 50px;">
-<p>Name: {{$data['name']}} </p>
-<p>Email: {{$data['email']}}</p>
-</div>
--- H071211030/Praktikum-7/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","","blog");
-
-if(!$con){
-    die('Connection Failed'. mysqli_connect_error());
-}
-
-?>
